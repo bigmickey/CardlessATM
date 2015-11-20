a91470e102cb72d16f3544d7fec46d10dc8af535
Michael <Michael@Michael-Dohertys-MacBook-Pro.local>
2015-11-20 12:27:34 +0200

    Skeleton App

---
+++ CardlessATM/GetCashViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GetCashViewController.swift
+//  CardlessATM
+//
+//  Created by Michael on 20/11/2015.
+//  Copyright © 2015 Michael. All rights reserved.
+//
+
+import UIKit
+
+class GetCashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
